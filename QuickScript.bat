78175859e749562e4b2ee728d03789646f654ec0
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2022-05-10 19:43:48 -0300

    Commit

---
+++ QuickScript.bat
@@ -0,0 +1,3 @@
+javac -d bin Teste.java Teste2.java
+cd bin
+jar --create --main-class Teste --file Teste.jar *
---
+++ Teste2.java
@@ -0,0 +1,9 @@
+public class Teste2{
+	
+	public static void main (String args[]){
+		System.out.println("sucesso2");
+		System.out.println("sucesso2");
+		
+	}
+	
+}
